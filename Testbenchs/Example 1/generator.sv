7426f8dc3084980e5c796961f443a815a64728cf
hungjuichangus-sketch <hungjuichang.us@gmail.com>
2026-04-14 17:54:06 -0700

    Update generator.sv

--- Testbenchs/Example 1/generator.sv
+++ Testbenchs/Example 1/generator.sv
@@ -4,7 +4,7 @@ class generator;
     mailbox #(reg_item) mbx;
     int num_tx = 10;
     
-    // NEW: A control knob for our tests to twist
+    // A control knob for our tests to twist
     string test_type = "base";
 
     function new(mailbox #(reg_item) mbx_arg);
@@ -15,17 +15,14 @@ class generator;
         for (int i = 0; i < num_tx; i++) begin
             reg_item req = new();
 
-            // NEW: The Test Plan Routing
+            // The Test Plan Routing
             if (test_type == "write_only") begin
-                // Inline constraint: Forces 'wr' to be 1, ignoring the 50/50 rule in reg_item
                 if (!req.randomize() with { wr == 1; }) $error("Rand failed");
             end 
             else if (test_type == "read_only") begin
-                // Inline constraint: Forces 'wr' to be 0
                 if (!req.randomize() with { wr == 0; }) $error("Rand failed");
             end 
             else begin
-                // The default base test uses the normal 50/50 reg_item rules
                 if (!req.randomize()) $error("Rand failed");
             end
 
