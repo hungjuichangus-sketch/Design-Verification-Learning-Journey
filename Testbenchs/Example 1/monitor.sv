874c77cc7edb8fdb8c2b6aabe0f1b701f746ca93
hungjuichangus-sketch <hungjuichang.us@gmail.com>
2026-04-14 17:54:41 -0700

    Update monitor.sv

--- Testbenchs/Example 1/monitor.sv
+++ Testbenchs/Example 1/monitor.sv
@@ -1,16 +1,9 @@
 class monitor;
 
-    // 1. Declare the virtual interface.
-    // The monitor needs to look at the pins, just like the driver.
     virtual reg_if vif;
-
-    // 2. Declare a parameterized mailbox.
-    // This mailbox will be used to send the observed data to the Scoreboard later.
     mailbox #(reg_item) scb_mbx;
     mailbox #(reg_item) cov_mbx;
-
-    // 3. The constructor.
-    // Pass the virtual interface and the mailbox in as arguments, just like you did for the driver.
+    
     function new(virtual reg_if vif_arg, mailbox #(reg_item) drv_mbx_arg,
                 mailbox #(reg_item) cov_mbx_arg);
         vif = vif_arg;
@@ -18,7 +11,6 @@ class monitor;
         cov_mbx = cov_mbx_arg;
     endfunction
 
-    // 4. The main run task.
     task run();
         forever begin
             reg_item tx;
@@ -36,21 +28,17 @@ class monitor;
                     tx.addr = vif.addr;
 
                     if (vif.wr == 1) begin
-                        // Writes complete instantly. Capture data now!
                         tx.wdata = vif.wdata;
 
                         $display("[%0t] [Monitor] Captured WRITE...", $time);
                         scb_mbx.put(tx);
                         cov_mbx.put(tx);
                     end else begin
-                        // Reads take extra cycles!
-                        // We must wait for the DUT to finish working and assert ready AGAIN.
                         do begin
                             @(posedge vif.clk);
                             #1;
                         end while (vif.ready == 0);
 
-                        // NOW the data on the wire is valid!
                         tx.rdata = vif.rdata;
 
                         $display("[%0t] [Monitor] Captured READ...", $time);
